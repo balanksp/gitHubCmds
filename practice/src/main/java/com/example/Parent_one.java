ea4d4b44dc647b649ba46e70b70f6d5f705f61d6
balanksp <a.bala101@gmail.com>
2023-02-13 10:27:43 +0530

    method add in branch

--- practice/src/main/java/com/example/Parent_one.java
+++ practice/src/main/java/com/example/Parent_one.java
@@ -1,7 +1,11 @@
 package com.example;
 
+
+
 public class Parent_one {
     // class create from the parent one class
-    
+    public void add(){
+        System.out.println("add method from child1 branch");
+    }
 
 }
