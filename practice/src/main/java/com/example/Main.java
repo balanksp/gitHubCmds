6cbfc4e338eeb7445c4a07184d03413a442910a6
balanksp <a.bala101@gmail.com>
2023-02-14 14:32:32 +0530

    changes happen

--- practice/src/main/java/com/example/Main.java
+++ practice/src/main/java/com/example/Main.java
@@ -2,11 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        // hi bala
-=======
-        // comment for make conflict
->>>>>>> fbb79b1c4cca2890b3ef3f2cfbff9810a42ccf42
+        // bala how are u ................
     }
-
+    
 }
